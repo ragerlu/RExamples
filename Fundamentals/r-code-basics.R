953b4aca6eae206f0cf7512a44aa127a87b42782
ragerlu <ragerlu@udel.edu>
2022-08-05 23:46:10 -0400

    update basics, for loops, while loops, functions, and if else files.
    Signed-off-by: ragerlu <ragerlu@udel.edu>

---
+++ Fundamentals/r-code-basics.R
@@ -0,0 +1,27 @@
+#The purpose of the r-code-basics.R file is to provide examples of the basics of the R code language. 
+
+#Arithmetic: The lines of code below can be typed into an R Interactive Terminal to produce the desired result which is displayed in a comment line below in []. 
+
+#Addition:
+1+1
+#[2]
+
+#Subtraction: 
+1-1
+#[0]
+
+#Division:
+2/1
+#[2]
+
+#Multiplication:
+2*2
+#[4]
+
+#Exponent Evaluation (Both methods):
+2**2
+2^2
+#[4]
+
+
+
