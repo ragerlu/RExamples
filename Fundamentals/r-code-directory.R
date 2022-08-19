566c16c34a223700bf372c3884ba7bc03a9db8e2
ragerlu <ragerlu@udel.edu>
2022-08-19 11:41:48 -0400

    Add and create directory file.

---
+++ Fundamentals/r-code-directory.R
@@ -0,0 +1,12 @@
+#The purpose of the r-code-directory.R file is to provide examples of directories and setting directories in the R code language. 
+#The lines of code below can be typed into an R Interactive Terminal to produce the desired result which is displayed in a comment line below in [].
+#These functions are used to navigate a system to find current location, set location with purpose to access data.
+
+#Get working directory:
+getwd()
+#Returns the folder path to your current location in the system.
+
+#Set working directory:
+setwd()
+#Sets your folder path to a desired location in the system.
+
