7bc2ad22b99369718f7ec7690191ab6116250cae
ragerlu <ragerlu@udel.edu>
2022-08-24 23:52:03 -0400

    update formatting of atomic file.

--- Fundamentals/r-code-atomic-vectors.R
+++ Fundamentals/r-code-atomic-vectors.R
@@ -1,7 +1,11 @@
-#The purpose of the r-code-atomic-vectors.R file is to provide examples of atomic vectors and their use in the R code language. 
-#The lines of code below can be typed into an R Interactive Terminal to produce the desired result which is displayed in a comment line below in [].
-#**A note on Attributes: An attribute is a piece of information that you can attach to an atomic vector or object in R. You can see which attributes
-#an object has with attributes. attributes will return NULL if an object has no attributes.**
+#The purpose of the r-code-atomic-vectors.R file is to provide examples
+#of atomic vectors and their use in the R code language.
+#The lines of code below can be typed into an R Interactive Terminal to
+#produce the desired result which is displayed in a comment line below in [].
+#**A note on Attributes: An attribute is a piece of information that you can
+#attach to an atomic vector or object in R. You can see which attributes
+#an object has with attributes. attributes will return NULL if an object has
+#no attributes.**
 
 #Atomic Vector: Simple vector of data.
 #Such as...:
@@ -11,11 +15,14 @@ die
 is.vector(die)
 #[True]
 
-#Each atomic vector stores its values as a one-dimensional vector, and each atomic vector can only store one type of data.
+#Each atomic vector stores its values as a one-dimensional vector, and each
+#atomic vector can only store one type of data.
 
-#R recognizes six basic types of atomic vectors: doubles, integers, characters, logicals, complex, and raw. 
+#R recognizes six basic types of atomic vectors: doubles, integers, characters,
+#logicals, complex, and raw.
 
-#R will do math with vectors that contain numbers and not with vectors that contain character strings. 
+#R will do math with vectors that contain numbers and not with vectors that
+#contain character strings.
 #For instance...:
 integ <-  c(1L, 3L)
 charac <- c("string1", "string2")
@@ -24,7 +31,8 @@ sum(integ)
 sum(charac)
 #[Error in sum(charac) : invalid 'type' (character) of argument]
 
-#Doubles: Stores regular numbers which can be positive or negative, large or small,  and have digits to the right of the 
+#Doubles: Stores regular numbers which can be positive or negative, large or
+#small,  and have digits to the right of the 
 #decimal place or not.
 #**Generally, R will save any numbers that you type in R as a double.**
 #**R functions refer to doubles as "numerics".**
@@ -35,16 +43,19 @@ die2
 typeof(die2)
 #["double"]
 
-#Integers: Stores numbers that can be written without a decimal component. 
-#R allows you to specifically  create an integer by typing a number followed by an uppercase L.
-#R will not save a number as an integer unless you include the L. Integer numbers without the L will be saved as doubles.  
+#Integers: Stores numbers that can be written without a decimal component.
+#R allows you to specifically  create an integer by typing a number followed
+#by an uppercase L.
+#R will not save a number as an integer unless you include the L. Integer
+#numbers without the L will be saved as doubles.
 #For instance...:
 integ2 <- c(2L, -4L, -6L, 7L)
 integ2
 #[2 -4 -6 7]
 
-#Characters: Stores small pieces of text. 
-#**The individual elemnts of a character vector are known as strings. A string can contain more than just letters. You can 
+#Characters: Stores small pieces of text.
+#**The individual elemnts of a character vector are known as strings. A string
+#can contain more than just letters. You can
 #assemble a character string from numbers or symbols as well.**
 text2 <- c("hello", "user")
 text2
@@ -52,7 +63,7 @@ text2
 typeof(text2)
 #"character"
 
-#Logicals: Stores TRUEs and FALSEs. 
+#Logicals: Stores TRUEs and FALSEs.
 #**R assumes that T and F are shorthand for TRUE and FALSE.**
 #For instance...:
 logicV <- c(TRUE, TRUE, FALSE)
@@ -63,7 +74,8 @@ typeof(logicV)
 typeof(T)
 #["logical"]
 
-#Complex and Raw: Doubles, integers, characters, and logicals are the most common types of atomic vectors in R, 
+#Complex and Raw: Doubles, integers, characters, and logicals are the most
+#common types of atomic vectors in R,
 #but R also recognizes two more types: complex and raw.
 #**To create a complex vector, add an imaginary term to a number with i.**
 #For instance...:
